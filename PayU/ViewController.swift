33a6bb6444f7c53657f2f834f436ffd9dd20fd87
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-05-09 22:58:33 +0530

    Initial Commit

---
+++ PayU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PayU
+//
+//  Created by Aayush Ranaut on 5/9/15.
+//  Copyright (c) 2015 Prathmesh Ranaut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
