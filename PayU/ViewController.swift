33850808a594941d42db847bd28a2c26380ae389
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-05-10 07:35:38 +0530

    Added JSON

--- PayU/ViewController.swift
+++ PayU/ViewController.swift
@@ -7,17 +7,69 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var graphView: GraphView!
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var tableView: UITableView!
     
-    var stocks:[String] = ["APPL", "GOGL", "FB"]
+    var stocks:[String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        let urlAsString = "http://localhost/payuapi/public/api/v1/stock/trending"
+//        let url = NSURL(string: urlAsString)
+//        let urlRequest = NSURLRequest(URL: url!)
+//        
+//        let queue = NSOperationQueue()
+//        
+//        NSURLConnection.sendAsynchronousRequest(urlRequest, queue: queue, completionHandler: { (response: NSURLResponse!,
+//            data: NSData!,
+//            error: NSError!) in
+//        
+//            
+//            if data.length > 0 && error == nil {
+//                let json = NSString(data: data, encoding: NSUTF8StringEncoding)
+//                let json2 = self.JSONParseArray(json)
+//                println("json = \(json2)")
+//            }
+//        })
+        
+        Alamofire.request(.GET, "http://10.100.86.148/payuapi/public/api/v1/stock/trending")
+            .responseJSON { (_, _, data, _) in
+                
+//                if let jsonResult = JSON as? Dictionary<String, Dictionary<String, Dictionary<String,String>>> {
+//                    println(jsonResult)
+//                }
+                
+                var json = JSON(data!)
+                
+                //println(json["data"])
+                
+                for (key: String, subJson: JSON) in json["data"] {
+                    self.stocks.append(subJson["name"].stringValue)
+                }
+                
+                println(self.stocks)
+                self.tableView.reloadData()
+        }
     }
+//
+//    func JSONParseArray(jsonString: String) -> [AnyObject] {
+//        if let data = jsonString.dataUsingEncoding(NSUTF8StringEncoding) {
+//        if let array = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0), error: nil)  as? [AnyObject] {
+//        return array
+//        }
+//        }
+//        return [AnyObject]()
+//    }
+    
+
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
