b35973595d555d73aeccbb3862754ebb4c776b9c
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-05-10 13:20:47 +0530

    Added ability to sell shares in portfolio

--- PayU/PortfolioViewController.swift
+++ PayU/PortfolioViewController.swift
@@ -14,6 +14,7 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
 
     var portfolioList: [AnyObject] = []
     var portfolioProfitList: [Float] = []
+    var tradeId: [Int] = []
     
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -28,17 +29,76 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
                 
                 println(json["data"])
                 
+                self.tradeId.removeAll(keepCapacity: false)
                 self.portfolioProfitList.removeAll(keepCapacity: false)
                 
                 for (key: String, subJson: JSON) in json["data"] {
                     self.portfolioList.append(subJson["name"].stringValue)
                     self.portfolioProfitList.append(subJson["profit"].floatValue)
+                    self.tradeId.append(subJson["id"].intValue)
                 }
                 
                 
                 println(self.portfolioList)
                 self.tableView.reloadData()
         }
+        
+        var lpgr: UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: Selector("handleLongPress:"))
+        lpgr.minimumPressDuration = 1.0
+        self.view.addGestureRecognizer(lpgr)
+    }
+    
+    func handleLongPress(gestureRecognizer: UILongPressGestureRecognizer) {
+        let longPress = gestureRecognizer as UILongPressGestureRecognizer
+        
+        let state = longPress.state
+        
+        var locationInView = longPress.locationInView(tableView)
+        
+        var indexPath = tableView.indexPathForRowAtPoint(locationInView)
+        
+        if !(indexPath != nil) {
+            println("Out of bounds")
+        } else if longPress.state == UIGestureRecognizerState.Began {
+            var alert = UIAlertController(title: "Sell", message: "Sell shares", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Yup!", style: UIAlertActionStyle.Default, handler: { action in
+                switch action.style{
+                case .Default:
+                    var url: String = "http://10.100.86.148/payuapi/public/api/v1/stock/sell/Bill%20Gates/" +  String(self.tradeId[indexPath!.row]);
+                    println(url)
+                    Alamofire.request(.GET, url)
+                        .responseJSON { (_, _, data, _) in
+                            
+                            self.portfolioList.removeAtIndex(indexPath!.row)
+                            self.tradeId.removeAtIndex(indexPath!.row);
+                            self.portfolioProfitList.removeAtIndex(indexPath!.row)
+                            
+                            println(self.portfolioList)
+                            self.tableView.reloadData()
+                    }
+                    
+                case .Cancel:
+                    println("cancel")
+                    
+                case .Destructive:
+                    println("destructive")
+                }
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { action in
+                switch action.style{
+                case .Default:
+                    println("default")
+                    
+                case .Cancel:
+                    println("cancel")
+                    
+                case .Destructive:
+                    println("destructive")
+                }
+            }))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -78,4 +138,5 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
         return portfolioList.count
     }
 
+    
 }
